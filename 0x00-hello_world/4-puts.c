5c689bc643a3e88b9c43d08913432e43f43450b7
abdul2k12 <abdul2k12.ak@gmail.com>
2023-03-16 13:37:36 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
-#!/bin/bash
-include<stdio h>
+#include <stdio.h>
 /**
 *main -Entry point
 *
@@ -9,3 +8,4 @@ int main(void){
   puts("\"Programming is like building a multilingual puzzle");
   return(0);
 }
+

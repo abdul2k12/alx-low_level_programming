a45584f6d9e93cf8f3b6a63447079d1d9fcf1909
abdul2k12 <abdul2k12.ak@gmail.com>
2023-03-20 13:15:07 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,23 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
- * main - Determine weather number is positive or negative or zero.
- *
- * Return: Always  0(success)
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
  */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-       if (n > 0) {
-            printf("%d n is a positive number ", n);
-       }else if (n == 0){
-         printf("%d n is zero ", n);
-       }else if (n < 0){
-         printf("%d n is negative ", n);
-         printf("\n");
-       }
-        
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
 }
